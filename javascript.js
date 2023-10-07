dc30ee60b4549a24d23ba95defdd66de82cf26fa
Raelinda Shoates <redfire809@gmail.com>
2023-10-07 10:51:01 -0400

    created info.html file and added content,styling and functionaility when user hits submit btn

--- javascript.js
+++ javascript.js
@@ -7,3 +7,10 @@
  toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+const myForm = document.getElementById("htmlForm");
+        const submitFunction = (e) => {
+            e.preventDefault();
+            window.location.href = "submit.html";
+        }
+        myForm.addEventListener("submit", submitFunction);
